d301aaaade049741c9d568e16fc87250b50c8785
tkdgns0630 <tkdgns0630@gmail.com>
2023-10-10 19:57:39 +1100

    created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+      <div className="Title-Subtitle">
+        San Franciscos Privately Owned Public Spaces
+      </div>
+    </div>
+  );
+}
+
+export default Title;
